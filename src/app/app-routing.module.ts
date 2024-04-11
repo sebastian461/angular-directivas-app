a6cf8122e2cd1edcc422f7e28f5c1dcfb1f7cb2d
sebastian461 <sebastiano-461@hotmail.com>
2024-04-11 12:21:23 -0500

    Estructura del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'directive',
     loadChildren: () =>
       import('./products/products.module').then((m) => m.ProductsModule),
   },
   {
+    path: 'signals',
+    loadChildren: () =>
+      import('./signals/signals.module').then((m) => m.SignalsModule),
+  },
+  {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'directive',
   },
 ];
 
---
+++ src/app/signals/layout/signals-layout/signals-layout.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './signals-layout.component.html',
+  styleUrl: './signals-layout.component.css',
+})
+export class SignalsLayoutComponent {}
