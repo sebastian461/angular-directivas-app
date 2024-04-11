bf99ac6e1c9b736bb70c042cd42ebcf250b3c34f
sebastian461 <sebastiano-461@hotmail.com>
2024-04-11 11:59:27 -0500

    Creación de la directiva

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 @NgModule({
   declarations: [ProductPageComponent],
-  imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class ProductsModule {}
