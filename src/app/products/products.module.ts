0d734d64d86806cdbd815471f6bccc0e70541a8b
sebastian461 <sebastiano-461@hotmail.com>
2024-04-11 11:02:01 -0500

    Formulario reactivo

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
+
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 
-
 @NgModule({
-  declarations: [
-    ProductPageComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
+  declarations: [ProductPageComponent],
+  imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
