d497feeaa6ffb6a5b0f7cf1b5ae97b2295a2a31f
sebastian461 <sebastiano-461@hotmail.com>
2024-04-12 16:35:34 -0500

    Mutación de señales y efectos

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interfaces';
 
 @Component({
   selector: 'selector-page',
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css',
 })
-export class PropertiesPageComponent {}
+export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 1,
+    email: 'sebastian@correo.com',
+    first_name: 'Sebastián',
+    last_name: 'Álava',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
+
+  public counter = signal(10);
+
+  public userChangedEffect = effect(() => {
+    console.log(`${this.user().first_name} - ${this.counter()}`);
+  });
+
+  onFieldUpdated(field: keyof User, value: string) {
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+      }
+
+      return current;
+    });
+  }
+}
